277b1f1b00610c17421ff17af43d8f5315550f44
Miguel Piza <mapiz_930@hotmail.com>
2024-10-04 16:37:44 -0500

    Fix sequence name and remove bad convertion

--- src/main/resources/db/migration/V7__add_conversions_table.sql
+++ src/main/resources/db/migration/V7__add_conversions_table.sql
@@ -4,10 +4,10 @@ START WITH 1
 ;
 
 CREATE TABLE IF NOT EXISTS conversions (
-    id INTEGER NOT NULL DEFAULT nextval('unit_sequence') PRIMARY KEY,
+    id INTEGER NOT NULL DEFAULT nextval('conversion_sequence') PRIMARY KEY,
     from_unit_id INTEGER, -- Unit from which the conversion happens (e.g. carton)
     to_unit_id INTEGER, -- Unit to which the conversion happens (e.g. unit)
-    conversion_factor DECIMAL(10, 5) NOT NULL, -- E.g. 1 carton = 30 units
+    conversion_factor DOUBLE PRECISION NOT NULL, -- E.g. 1 carton = 30 units
     FOREIGN KEY (from_unit_id) REFERENCES units(id),
     FOREIGN KEY (to_unit_id) REFERENCES units(id)
 );
@@ -16,7 +16,6 @@ CREATE TABLE IF NOT EXISTS conversions (
 INSERT INTO conversions (from_unit_id, to_unit_id, conversion_factor)
 VALUES
 (1, 2, 30.00),      -- 1 carton = 30 units
-(2, 1, 23.00),      -- 1 carton = 30 units
 (3, 2, 15.00),      -- 1 half carton = 15 units
 (7, 2, 6.00),       -- 1 milk pack = 6 units
 (5, 4, 1000.00),    -- 1 kilogram = 1000 grams
